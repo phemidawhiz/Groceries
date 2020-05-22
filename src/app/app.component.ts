aab1f2f0dac1cfe4f247889f463ace77cc3c6996
phemidawhiz <feminiscence@gmail.com>
2020-05-22 18:50:20 +0100

    StackLayout and global css

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,15 @@ import { Component } from "@angular/core";
 
 @Component({
     selector: "gr-main",
-    template: "<Label text='hello world'></Label>"
+    template: `
+        <StackLayout>
+            <TextField hint="Email Address" keyboardType="email"
+            autocorrect="false" 
+            autocapitalisationType="none"></TextField>
+
+            <Button text="Sign In"></Button>
+            <Button text="Sign Up For Groceries"></Button>
+        </StackLayout>
+    `
 })
 export class AppComponent { }
